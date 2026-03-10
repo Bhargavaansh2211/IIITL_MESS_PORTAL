3c969e423b2675f9a5eddaa4f12cea9230fbf6e5
Bhargavaansh2211 <143218592+Bhargavaansh2211@users.noreply.github.com>
2026-03-10 16:44:13 +0530

    created route for user payment

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const cors = require('cors');
 const express = require('express');
+const mongoose = require('mongoose');
 const compression = require('compression');
 const dotenv = require('dotenv');
+const session = require('express-session');
+const helmet = require('helmet');
 
-
+const MongoStore = require('connect-mongo')(session);
 dotenv.config({ path: './config/config.env' });
 
 const PORT = process.env.PORT || 3000;
@@ -11,6 +14,14 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 const FRONTEND_DIR = __dirname + '/frontend/build';
 
 const app = express();
+
+app.set('trust proxy', 1);
+
+// DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,6 +34,62 @@ app.use(
   })
 );
 
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      useDefaults: true,
+      directives: {
+        "default-src": ["'self'"],
+        "script-src": [
+          "'self'",
+          "'unsafe-inline'",
+          "'wasm-unsafe-eval'",
+          "'unsafe-eval'",
+          "https://checkout.razorpay.com",
+          "https://cdn.jsdelivr.net",
+          "https://fastly.jsdelivr.net"
+        ],
+        "connect-src": [
+          "'self'",
+          "https://api.razorpay.com",
+          "https://checkout.razorpay.com",
+          "https://cdn.jsdelivr.net",
+          "https://fastly.jsdelivr.net"
+        ],
+       
+        "frame-src": ["'self'", "https://*.razorpay.com"],
+        "img-src": ["'self'", "data:", "https://*.razorpay.com"],
+        "style-src": ["'self'", "'unsafe-inline'"],
+        "media-src": ["'self'", "data:"],
+        "worker-src": ["'self'", "blob:"]
+      }
+    },
+    crossOriginEmbedderPolicy: false,
+    referrerPolicy: { policy: "no-referrer" },
+    permissionsPolicy: {
+      features: {
+        camera: ["self"],
+        microphone: ["self"]
+      }
+    }
+  })
+);
+
+app.use(
+  session({
+    secret: 'IIITL MESS PORTAL',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      sameSite: 'lax',
+      secure: NODE_ENV === 'production',
+    },
+    store:
+      new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+app.use('/api/user', require('./routes/user'));
 
 app.use(express.static(FRONTEND_DIR));
 app.get('*', (req, res) => res.sendFile(FRONTEND_DIR + '/index.html'));
